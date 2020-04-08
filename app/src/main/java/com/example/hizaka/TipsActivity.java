02c114af50509ffac3f3a625abf6b3afd8f743c6
hakoriku <62737271+hakoriku@users.noreply.github.com>
2020-04-09 02:56:07 +0900

    ちょっといじりました
    画面遷移でわからないことがあれば聞いてください。新しく作ったActivityのBottomNavigationは自分が最後にプログラムするので、なしで作っておいて構いません。Tipsページは自分が作ります。

--- app/src/main/java/com/example/hizaka/TipsActivity.java
+++ app/src/main/java/com/example/hizaka/TipsActivity.java
@@ -1,33 +1,44 @@
 package com.example.hizaka;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import java.util.ArrayList;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-public class MainActivity extends AppCompatActivity
-    implements View.OnClickListener
+public class TipsActivity extends AppCompatActivity
+        implements View.OnClickListener
 {
-    @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_tips);
+        // ListViewに表示するリスト項目をArrayListで準備
+        ArrayList data = new ArrayList<>();
+        data.add("How to use trains");
+        data.add("How to use Buses");
+        data.add("How to use public phone");
+        data.add("Go to hosipital");
+        data.add("Something was stolen");
+        data.add("Manner of eat");
+        // リスト項目とListViewを対応付けるArrayAdapterを用意する
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        // ListViewにArrayAdapterを設定する
+        ListView listView = (ListView)findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
         findViewById(R.id.navigation_home).setOnClickListener(this);
         findViewById(R.id.navigation_train).setOnClickListener(this);
         findViewById(R.id.navigation_tips).setOnClickListener(this);
         findViewById(R.id.navigation_emergency).setOnClickListener(this);
-
     }
+
+
+
     public void onClick1()
     {
         Intent intent = new Intent(this, MainActivity.class);  //インテントの作成
@@ -49,6 +60,7 @@ public class MainActivity extends AppCompatActivity
         startActivity(intent);                                 //画面遷移
     }
 
+    @Override
     public void  onClick(View v)
     {
         switch (v.getId())
@@ -65,8 +77,6 @@ public class MainActivity extends AppCompatActivity
             case R.id.navigation_emergency:
                 onClick4();
                 break;
-
         }
     }
-
 }
